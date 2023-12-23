2108e48e44b40b617fb13afea30e91c037c0df19
Tigran Yeghiazaryan <tigraneghiazaryan24@gmail.com>
2023-12-23 12:59:42 +0400

    First Python codes

---
+++ main.py
@@ -0,0 +1,116 @@
+# a = 8
+# b = 12
+#
+# print(a,b)
+#
+# a = a + b
+# b = a - b
+# a = a - b
+#
+# print(a,b)
+import random
+
+
+# num = 8
+#
+# if num > 0:
+#     print("false")
+# elif num < 0:
+#     print("true")
+
+# num = input("Enter the number")
+# num = int(num)
+#
+# if num > 0:
+#     print("false")
+# elif num < 0:
+#     print("true")
+
+# Even, Odd
+
+# num = int(input("Enter a number: "))
+#
+# if num % 2 == 0:
+#     print("Even")
+# elif num % 2 == 1:
+#     print("Odd")
+
+# num = int(input("Enter a number: "))
+#
+#
+# for i in range(num):
+#     if i % 2 == 0:
+#         print(i)
+#
+
+
+
+# Remove Chars
+# def rem_chars(s, n):
+#
+#     result = s[n:]
+#     return result
+#
+# print(rem_chars('Umbrella', 6))
+
+
+
+
+
+# str_x = "Emma is good developer. Emma is a writer"
+#
+# def count_str(w, str):
+#     return str.count(w)
+#
+# print(count_str('Emma', str_x))
+
+# X2 Array numbers
+# arr = [1, 2, 3, 4, 5, 6]
+#
+# def x2_arr(array):
+#     arr2 = []
+#     for i in array:
+#         arr2.append(i*2)
+#
+#     return arr2
+#
+# print(x2_arr(arr))
+
+
+# arr = [1, 10, 400, 7, -1]
+#
+# def k_plus(array, k):
+#
+#     for i in range(len(array)):
+#         for j in range(i + 1, len(array)):
+#
+#             if array[i] + array[j] == k:
+#                 return array[i], array[j]
+#
+#
+#     return 'no'
+#
+#
+# print(k_plus(arr, 406))
+
+# Random Number from x to y
+
+# def random_n(rang_min, rang_max):
+#     return random.randint(rang_min, rang_max)
+#
+#
+# print(random_n(1, 100))
+
+
+
+# arr = [1, 45, 636, 353, 55]
+# def xx(array, k):
+#     for i in array:
+#         for j in range(i + 1):
+#             if(array[i] + array[j] == k):
+#                 return [array[i], array[j]]
+#
+#
+#
+# print(xx(arr, 46))
+
